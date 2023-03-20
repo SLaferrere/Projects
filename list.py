a5dc2eb7812ae367d245cec01a94b0cf5af33372
SLaferrere <laferreresantiago@gmail.com>
2023-03-20 12:36:49 -0300

    add file creator

--- list.py
+++ list.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 request = requests.get("https://api.mercadolibre.com/sites/MLA/search?category=MLA1051").json()
 
@@ -8,11 +9,28 @@ def average_price():
     amount = 0
     for number_of_models in request["results"]:
         if count <= 25:
-            price_of_module = number_of_models["price"]
-            amount += price_of_module
+            price_of_model = number_of_models["price"]
+            amount += price_of_model
             count += 1
         else:
             average = round(amount / count, 2)
             return average
-        
+
+# genera un excel con los primeros 25 modelos, condicion y precio de cada uno
+def get_models():
+    count = 0
+    models = {"Models": [],
+              "Condition": [],
+              "Price": []}
+    for models_names in request["results"]:
+        if count <= 25:
+            models["Models"].append(models_names["title"])
+            models["Condition"].append(models_names["condition"])
+            models["Price"].append(models_names["price"])
+            count += 1
+        else:
+            df = pd.DataFrame(models)
+            df.to_excel("Models.xlsx", index = False)
+
+get_models()
 print(average_price())
