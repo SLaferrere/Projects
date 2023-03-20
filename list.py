563065965fa582078017d92f874eb223d0fe6271
SLaferrere <laferreresantiago@gmail.com>
2023-03-20 18:22:44 -0300

    formating code

--- list.py
+++ list.py
@@ -1,10 +1,9 @@
 import requests
 import pandas as pd
 
-request = requests.get("https://api.mercadolibre.com/sites/MLA/search?category=MLA1051").json()
-
 # toma los primeros 25 resultados de celulares y devuelve el promedio de precios
 def average_price():  
+    request = requests.get("https://api.mercadolibre.com/sites/MLA/search?category=MLA1051").json()
     count = 0
     amount = 0
     for number_of_models in request["results"]:
@@ -18,19 +17,19 @@ def average_price():
 
 # genera un excel con los primeros 25 modelos, condicion y precio de cada uno
 def get_models():
+    request = requests.get("https://api.mercadolibre.com/sites/MLA/search?category=MLA1051").json()
     count = 0
-    models = {"Models": [],
-              "Condition": [],
-              "Price": []}
+    models = []
     for models_names in request["results"]:
         if count <= 25:
-            models["Models"].append(models_names["title"])
-            models["Condition"].append(models_names["condition"])
-            models["Price"].append(models_names["price"])
+            model = {}
+            model["Model"] = models_names.get("title", "")
+            model["Condition"] = models_names.get("condition", "")
+            model["Price"] = models_names.get("price", "")
+            models.append(model)
             count += 1
         else:
-            df = pd.DataFrame(models)
-            df.to_excel("Models.xlsx", index = False)
+            pd.DataFrame(models).to_excel("Models.xlsx", index = False)
 
 get_models()
 print(average_price())
